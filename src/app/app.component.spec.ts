507795c541eed98e860eb43b1a5aa04cc2b5862d
Samuel Laroche <samuellaroche@live.ca>
2019-12-01 07:17:41 -0500

    upgraded to angular 8

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('coincove-ui');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to coincove-ui!');
+    expect(compiled.querySelector('.content span').textContent).toContain('coincove-ui app is running!');
   });
 });
